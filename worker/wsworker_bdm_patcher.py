a934f8983f4cad7d9dc90182eebf1653af27815b
xfork <liuheng0204@gmail.com>
2013-06-27 11:06:03 +0800

    bb

--- worker/wsworker_bdm_patcher.py
+++ worker/wsworker_bdm_patcher.py
@@ -1,4 +1,4 @@
-# coding=UTF-8
+﻿# coding=UTF-8
 """
 @author thomas
 @date    2013-03-31
@@ -54,7 +54,7 @@ def main(argc, argv):
 	
 	#init constants
 	timeout_buildserver = 0
-	workerId = 'd160ad29-80d3-4b22-95a1-61535d14e8ac'
+	workerId = '%s' % uuid.uuid4()
 	nickname = 'BDMPatcher'
 	
 	while True:
