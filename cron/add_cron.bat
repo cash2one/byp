fc05db361f07a7abebce603e6107fcc97db3beb1
xfork <liuheng0204@gmail.com>
2014-01-03 13:02:48 +0800

    update

--- cron/add_cron.bat
+++ cron/add_cron.bat
@@ -1,2 +1,9 @@
 at 00:00 /every:M,T,W,Th,F,S,Su python F:\BDMTrunk\byp\cron\cron_client.py mgr_full_without_tag
 at 00:00 /every:M,T,W,Th,F,S,Su python F:\BDMTrunk\byp\cron\cron_client.py bdkv_release_without_tag
+at 00:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_kvadapt
+at 01:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_bdmadapt
+at 02:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_combine
+at 03:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_combine_bdkv
+at 04:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_combine_bdm
+at 05:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_bdkv
+at 06:00 /every:M,T,W,Th,F,S,Su python E:\XBUILD\byp\cron\cron_client.py daily_bdm
